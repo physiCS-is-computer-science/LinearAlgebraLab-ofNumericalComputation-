1f0e7189e2c68d68df6cff193b0529486bc9f953
MisakaMikoto <2607667600@qq.com>
2025-11-09 23:23:43 +0800

    ### 更新了一点命令内容 feat: - 给 help 命令增加了许多内容

--- LaxbIO/include/cmdlib.hpp
+++ LaxbIO/include/cmdlib.hpp
@@ -4,8 +4,8 @@
 
 #pragma once
 
-#include <string>
 #include <map>
+#include <string>
 #include <utility>
 
 namespace laxb {
@@ -21,7 +21,18 @@ const std::string clsMan{"\tClear screen"};
 const std::string helpMan{"-a:\tDisplay all commands\n-l:\tDisplay a list of all command names\n:cmdname:\tDisplay specific command details"};
 
 /* ==== 运算 ==== */
-//
+const std::string plusMan{":varn:\tAccept a variable\n\t:varn :varn\n::varn:\tThe result is output to this variable"};
+const std::string minusMan{":varn:\tAccept a variable\n\t:varn :varn\n::varn:\tThe result is output to this variable"};
+const std::string mtimesMan{":varn:\tAccept a variable\n\t:varn :varn\n::varn:\tThe result is output to this variable"};
+const std::string timesMan{":varn:\tAccept a variable\n\t:varn :varn\n::varn:\tThe result is output to this variable"};
+const std::string divideMan{":varn:\tAccept a variable\n\t:varn :varn\n::varn:\tThe result is output to this variable"};
+const std::string powerMan{":varn:\tAccept a variable\n\t:varn :varn\n::varn:\tThe result is output to this variable"};
+
+/* ==== 矩阵创建与操作 ==== */
+
+/* ==== 矩阵分解 ==== */
+const std::string luMan{"-a:\tThe output includes all the results of L, U, P\n:varn:\tInput matrix\n::varn:\tSave the output to this variable\n\t::L ::U\n\t::L ::U ::P"};
+const std::string rrefMan{":varn:\tInput matrix\n::varn:\tSave the output to this variable"};
 
 const std::map<std::string, std::pair<std::string, std::string>> manual{
     /* ==== 基本命令 ==== */
@@ -35,6 +46,18 @@ const std::map<std::string, std::pair<std::string, std::string>> manual{
     {"help", {":cmdname", helpMan}},
 
     /* ==== 运算 ==== */
+    {"plus", {":varn :varn ::varn", plusMan}},
+    {"minus", {":varn :varn ::varn", minusMan}},
+    {"mtimes", {":varn :varn ::varn", mtimesMan}},
+    {"times", {":varn :varn ::varn", timesMan}},
+    {"divide", {":varn :varn ::varn", divideMan}},
+    {"power", {":varn :varn ::varn", powerMan}},
+
+    /* ==== 矩阵创建与操作 ==== */
+
+    /* ==== 矩阵分解 ==== */
+    {"lu", {"-a :varn ::varn", luMan}},
+    {"rref", {":varn ::varn", rrefMan}},
 };
 
 } // namespace laxb
